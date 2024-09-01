540f084b5dbeb6659d7a8aa0e70120c83f4e83fa
Jerruz <jerruz@jerruz.com>
2024-09-01 19:57:56 +0300

    part 1 solved

---
+++ part_1_js/script.js
@@ -0,0 +1,96 @@
+// ==============
+// task 1
+// ==============
+
+// var a = 5;
+// let b = 10;
+// const c = 13;
+
+// console.log(a, b, c);
+
+// [a, b] = [11, 22];
+
+// console.log(a, b);
+
+// ==============
+// task 2
+// ==============
+
+
+// const a = 100;
+// a = 20;
+// console.log(a);
+
+// console.log(a);
+// let a = 15;
+// console.log(a);
+
+// console.log(a);
+// var a;
+// a = 10;
+// console.log(a);
+
+
+// ==============
+// task 3
+// ==============
+
+// let productName = 'apple';
+// let productPrice = 500;
+// let productQuantity = 3;
+// let resultCost = productPrice * productQuantity;
+
+// console.log(`Вы купили ${productName} ${productQuantity} шт. по ${productPrice} ₽. Всего было потрачено: ${resultCost} ₽`);
+
+
+// ==============
+// task 4
+// ==============
+
+
+// console.log(typeof 'Hello');
+// console.log(typeof 10);
+// console.log(typeof {});
+// console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof true);
+// console.log(typeof []);
+// console.log(typeof (() => {}));
+
+
+// ==============
+// task 5
+// ==============
+
+
+// console.log(10 == 10)
+// console.log(10 != 10)
+// console.log(12 == 'Привет')
+// console.log(10 < 11)
+// console.log(12 > 20)
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && true)
+// console.log(false || true)
+// console.log(true || false)
+// console.log(true || true)
+// console.log(10 <= 10)
+// console.log(18 <= 20)
+// console.log(50 >= 50)
+// console.log(51 >= 50)
+// console.log({} === {})
+// console.log({} == {})
+
+// const a = {};
+// const b = a;
+// console.log(a === b);
+
+
+// ==============
+// task 6
+// ==============
+
+let a = 31;
+let b = 5;
+
+console.log(a > b && true || false)
